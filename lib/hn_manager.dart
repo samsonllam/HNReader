3448cc55dc53aa11e63822416f79997f810cbcf4
samsonllam <fongpuilam@gmail.com>
2019-03-22 17:32:22 +0800

    fetch story content

--- lib/hn_manager.dart
+++ lib/hn_manager.dart
@@ -5,16 +5,29 @@ import 'package:http/http.dart' as http;
 
 import './storyList.dart';
 
-Future<List<String>> fetchTopNews() async {
+Future<List<String>> fetchTopNewsId() async {
   http.Response response = await http.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'); 
 
-  List<String> topStoryList = response.body.substring(1, response.body.length-2).split(", ");
+  List<String> topStoryList = response.body.substring(2, response.body.length-2).split(", ").take(20).toList();
 
   if (response.statusCode == 200) {
     return topStoryList;
   } else {
     throw Exception('Failed to load top stories');
   }
+}
+
+Future<List<String>> fetchNewsStory() async {
+  List<String> topNewsId = await fetchTopNewsId();
+  List<String> topNewsStory;
+
+
+  for (var id in topNewsId) {
+  http.Response response = await http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json?print=pretty'); 
+    topNewsStory.add(response.body);
+  }
+
+  return topNewsStory;
 }
 
 class HNManager extends StatefulWidget {
@@ -30,6 +43,7 @@ class HNManager extends StatefulWidget {
 class _HNManagerState extends State<HNManager> {
   @override
   Widget build(BuildContext context) {
-    return StoryList(fetchTopNews());
+    return StoryList(fetchNewsStory());
+    
   }
 }
