05a8efef562d58ac92c46cbbb79766e015c34a41
samsonllam <fongpuilam@gmail.com>
2019-04-07 16:10:35 +0800

    changed to bottomNavigationBar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,40 +1,41 @@
 import 'package:flutter/material.dart';
 import './itemlist.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
 
+class MyHomePage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HNReader',
+      home: _MyTabBar(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
+class _MyTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          _selectedIndex == 0? ItemList() : new Text('hi')
-        ]
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.rss_feed), title: Text('Top Stories')),
-          // BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('Job')),
-          BottomNavigationBarItem(icon: Icon(Icons.star), title: Text('Star')),
-        ],
-        currentIndex: _selectedIndex,
-        fixedColor: Colors.deepPurple,
-        onTap: _onItemTapped,
-      ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.list), text: 'Top Stories'),
+            Tab(icon: Icon(Icons.rss_feed), text: 'Top Stories'),
+          ],
+          unselectedLabelColor: Color(0xff999999),
+          labelColor: Colors.deepPurple,
+          indicatorColor: Colors.transparent,
+        ),
+        body: TabBarView(
+          children: [
+            Column(children: [ItemList()],),
+            Center( child: Text('page2'),),
+          ]
+        )
+      )
     );
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 }
